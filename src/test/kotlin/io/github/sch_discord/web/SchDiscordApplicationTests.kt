52f4d1709625aefdc541ae551d8e2be3ee5c6655
found-cake <skymin468@gmail.com>
2024-02-13 09:58:38 +0900

    init

---
+++ src/test/kotlin/io/github/sch_discord/web/SchDiscordApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.sch_discord.web
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchDiscordApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
